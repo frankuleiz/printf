f545471af8b8e021d2b3e422e27a0687332f1d67
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-25 13:24:19 +0300

    updated code for get_print.c

--- get_print.c
+++ get_print.c
@@ -1,28 +1,39 @@
 #include "main.h"
+
 /**
- *get_print - selects the right printing function
- *@s: conversion specifier
- *Return: a pointer to the matching printing function
+ * get_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * func_arr[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
  */
-int (*get_print(char *s))(va_list, flags_t *)
+int (*get_print(char s))(va_list, flags_t *)
 {
 	ph func_arr[] = {
+		{'i', print_int},
 		{'s', print_string},
 		{'c', print_char},
 		{'d', print_int},
-		{'i', print_int}
-		{'b', print_binary}
-	}
-	int flags = 5;
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int flags = 14;
 
 	register int i;
 
 	for (i = 0; i < flags; i++)
-	{
-		if (func_arr[i].c == *s)
-		{
+		if (func_arr[i].c == s)
 			return (func_arr[i].f);
-		}
-	}
 	return (NULL);
-}
+}i
