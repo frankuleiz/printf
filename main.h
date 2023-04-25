fe4b1ec90a1ca5452456232e9d7d695b54c433f0
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-04-25 14:40:41 +0300

    correction

--- main.h
+++ main.h
@@ -46,7 +46,6 @@ int _putchar(char c);
 int _puts(char *str);
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list r);
-int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
 int print_address(va_list l, flags_t *f);
 int print_percent(va_list l, flags_t *f);
